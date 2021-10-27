3bbbe80efb6860774fb32c742f82b1006c5dbe6f
negin <83733021+negindaryadel@users.noreply.github.com>
2021-10-27 10:30:02 +0330

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,46 +1,21 @@
 #include <iostream>
-#include<time.h>
-#include <cstdlib>
+#include <math.h>
 
 using namespace std;
 
 int main()
 {
-  string words[3] = { "computer","console","desktop" };
-   string word , userinput[12];
-   int random;
-   char letter;
-   srand(time(0));
-   random= rand()%3;
-   word=words[random];
-   for (int i = 0; i < word.size(); i++) {
-		cout << "-" << '\t';
-	}
-	cout<<endl << "enter letter"<<endl;
-	cin>>letter;
-	for (int i = 0; i < word.size(); i++) {
-		if (letter == word[i]) {
-			userinput[i] = letter;
-		}
-	}
-	for (int i = 0; i < word.size(); i++)
-	{
-		cout << userinput;
-
-
-	if (word == userinput[i]) {
-		break;
-	}
-}
-   if (word == userinput) {
-    cout << "\nYou won!" << endl;
+    int n;
+    cout<<"please enter size snake"<<endl;
+    cin>>n;
+    for(int i=0 ; i<n/2;i++)
+    {
+        cout<<"*#";
     }
+    if (n%2 !=0 )
+    {
 
-	else  cout <<"\n"<<word <<"\ngameover!" << endl;
-
-
-
-	return 0;
-
+        cout<<"*";
+    }
+    return 0;
 }
-
