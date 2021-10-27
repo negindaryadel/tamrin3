0a4e1eae4fb7b6fcda525ecc3f3cc99544a305c4
negin <83733021+negindaryadel@users.noreply.github.com>
2021-10-27 11:03:04 +0330

    Add files via upload

---
+++ random_array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "enter n for random-array:  ";
+    int n;
+    cin>>n;
+    int random_array[30];
+      srand(time(NULL));
+    for(int i=0; i<n; ++i)
+    {
+
+      random_array[i]=rand()%31;
+
+
+}
+    cout << "["; {
+		for (int i = 0; i < n ; i++)
+			cout << random_array[i] << " ";
+
+			}
+	cout <<  "]";
+
+
+    return 0;
+}
